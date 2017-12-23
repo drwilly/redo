17f28504846eb3473ff6d406f458b7e7ba015e3b
Wilke Schwiedop <drwilly@drwilly.de>
2017-12-23 13:44:54 +0100

    add 'extern'

--- src/environment.h
+++ src/environment.h
@@ -1,9 +1,9 @@
 #ifndef _ENVIRONMENT_H
 #define _ENVIRONMENT_H
 
-int            redo_getenv_int(const char *k, int def);
-void           redo_setenv_int(const char *k, int v); 
-const char *   redo_getenv_str(const char *k, const char *def);
-void           redo_setenv_str(const char *k, const char *v); 
+extern int            redo_getenv_int(const char *k, int def);
+extern void           redo_setenv_int(const char *k, int v);
+extern const char *   redo_getenv_str(const char *k, const char *def);
+extern void           redo_setenv_str(const char *k, const char *v);
 
 #endif
